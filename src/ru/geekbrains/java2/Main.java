2723db71abcd501c5b1e44dbee52cb031e13ca77
maks6169 <maks6169@rambler.ru>
2022-06-06 17:47:24 +0300

    Initial commit

---
+++ src/ru/geekbrains/java2/Main.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.java2;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList("one", "two", "three", "one", "one", "four", "five", "five", "six", "seven"));
+        System.out.println(arr);
+        HashMap<String, Integer> newArr = new HashMap<>();
+        for (String s : arr) {
+            newArr.merge(s, 1, Integer::sum);
+        }
+        System.out.println(newArr);
+        TreeSet<String> arr1 = new TreeSet<>(arr);
+        System.out.println(arr1);
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Pupkin", "1234");
+        phoneBook.add("Sumkin", "2345");
+        phoneBook.add("Beggins", "3456");
+        phoneBook.add("Sumkin", "4567");
+        phoneBook.add("Sumkin", "5678");
+        System.out.println("Sumkin phone" + phoneBook.get("Sumkin"));
+        System.out.println("Pupkin phone" + phoneBook.get("Pupkin"));
+    }
+
+}
+
+
---
+++ src/ru/geekbrains/java2/PhoneBook.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.java2;
+
+import java.util.*;
+
+public class PhoneBook {
+    private Map<String, List<String>> phoneBook = new HashMap<>();
+    private List<String> phoneNumberList;
+
+    public void add(String name, String phoneNumber) {
+        if (phoneBook.containsKey(name)){
+        phoneNumberList = phoneBook.get(name);
+        }else {
+            phoneNumberList = new ArrayList<>();
+        }
+        phoneNumberList.add(phoneNumber);
+        phoneBook.put(name,phoneNumberList);
+    }
+    public List<String> get(String name){
+        return phoneBook.get(name);
+    }
+}
+
